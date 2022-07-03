b139703edda461dfcd1f082af163629f3b60935a
iamarthur <arthurkyambadde9@gmail.com>
2022-07-03 13:27:04 +0300

    splitting of components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   // const expenseDate = new Date(2020, 0, 1);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 298.45;
 
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
       <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
+        <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
